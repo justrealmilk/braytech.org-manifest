9d1a139f1ad9df0eb6d7a3127f8240d380f8034a
Tom Chapman <chapman@live.com.au>
2023-02-16 19:07:03 +1000

    ...

--- ja/index.ts
+++ ja/index.ts
@@ -1,9 +1,11 @@
 import Braytech from '@Data/manifest/ja/Braytech.json';
+import BraytechActivityDifficulty from '@Data/manifest/ja/BraytechActivityDifficulty.json';
 import BraytechMaps from '@Data/manifest/ja/BraytechMaps.json';
 import BraytechMapsTypes from '@Data/manifest/ja/BraytechMapsTypes.json';
 import DestinyActivityDefinition from '@Data/manifest/ja/DestinyActivityDefinition.json';
 import DestinyActivityModeDefinition from '@Data/manifest/ja/DestinyActivityModeDefinition.json';
 import DestinyActivityModifierDefinition from '@Data/manifest/ja/DestinyActivityModifierDefinition.json';
+import DestinyActivityTypeDefinition from '@Data/manifest/ja/DestinyActivityTypeDefinition.json';
 import DestinyChecklistDefinition from '@Data/manifest/ja/DestinyChecklistDefinition.json';
 import DestinyCollectibleDefinition from '@Data/manifest/ja/DestinyCollectibleDefinition.json';
 import DestinyDamageTypeDefinition from '@Data/manifest/ja/DestinyDamageTypeDefinition.json';
@@ -24,11 +26,13 @@ import DestinyTraitDefinition from '@Data/manifest/ja/DestinyTraitDefinition.jso
 const ja = {
   definitions: {
     Braytech,
+    BraytechActivityDifficulty,
     BraytechMaps,
     BraytechMapsTypes,
     DestinyActivityDefinition,
     DestinyActivityModeDefinition,
     DestinyActivityModifierDefinition,
+    DestinyActivityTypeDefinition,
     DestinyChecklistDefinition,
     DestinyCollectibleDefinition,
     DestinyDamageTypeDefinition,
