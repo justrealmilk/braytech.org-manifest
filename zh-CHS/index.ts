031ead5d1b16f94e516398e4fefcc1b52eac7ebb
OV3RWR1T3_ <33184021+FR0Z3@users.noreply.github.com>
2023-04-20 04:14:21 +0800

    Updating some things for zh-CHS (#219)
    * Update DestinyChecklistDefinition(zh-CHS)
    
    * Preping FeatureDefinition for the future
    
    Preping this file for further translation.
    
    * Update DestinyInventoryItemDefinitionColloquial.json
    
    * Add files via upload
    
    * Update index.ts
    
    * Add files via upload
    
    * Update index.ts
    
    * Adding App ActivityDefinition (Translation WIP)
    
    * Update index.ts
    
    * Adding DestinyActivityDefinition (Translation WIP)
    
    * Update index.ts
    
    * Adding DestinyActivityModeDefinition (Localiz.WIP)
    
    * Update index.ts
    
    * Adding DestinyStatDefinition
    
    * Update DestinyStatDefinition.json
    
    * Update index.ts
    
    * Update index.ts
    
    * Adding InventoryItemDefinition
    
    * Update App FeatureDefinition (Translation WIP)
    
    * Making ActivitivityDefenition Cleaner
    
    * Cleaning up DestinyActivityDefinition
    
    * Fixing a typo in InventoryItemDefinition

--- zh-CHS/index.ts
+++ zh-CHS/index.ts
@@ -1,34 +1,53 @@
 import BraytechActivityDifficultyDefinition from '@Data/manifest/zh-CHS/BraytechActivityDifficultyDefinition.json';
+import BraytechActivityDefinition from '@Data/manifest/zh-CHS/BraytechActivityDefinition.json';
 import BraytechCommonDefinition from '@Data/manifest/zh-CHS/BraytechCommonDefinition.json';
 import BraytechFeatureDefinition from '@Data/manifest/zh-CHS/BraytechFeatureDefinition.json';
 import BraytechMapTypesDefinition from '@Data/manifest/zh-CHS/BraytechMapsTypesDefinition.json';
 import BraytechRotationDefinition from '@Data/manifest/zh-CHS/BraytechRotationDefinition.json';
+import DestinyActivityDefinition from '@Data/manifest/zh-CHS/DestinyActivityDefinition.json';
+import DestinyActivityModeDefinition from '@Data/manifest/zh-CHS/DestinyActivityModeDefinition.json';
 import DestinyActivityModifierDefinition from '@Data/manifest/zh-CHS/DestinyActivityModifierDefinition.json';
 import DestinyChecklistDefinition from '@Data/manifest/zh-CHS/DestinyChecklistDefinition.json';
 import DestinyCollectibleDefinition from '@Data/manifest/zh-CHS/DestinyCollectibleDefinition.json';
+import DestinyDamageTypeDefinition from '@Data/manifest/zh-CHS/DestinyDamageTypeDefinition.json';
 import DestinyHistoricalStatsDefinition from '@Data/manifest/zh-CHS/DestinyHistoricalStatsDefinition.json';
+import DestinyInventoryItemDefinition from '@Data/manifest/zh-CHS/DestinyInventoryItemDefinition.json';
 import DestinyInventoryBucketDefinition from '@Data/manifest/zh-CHS/DestinyInventoryBucketDefinition.json';
-import DestinyInventoryItemDefinitionColloquial from '@Data/manifest/zh-CHS/DestinyInventoryItemDefinitionColloquial.json';
 import DestinyMilestoneDefinition from '@Data/manifest/zh-CHS/DestinyMilestoneDefinition.json';
+import DestinySeasonDefinition from '@Data/manifest/zh-CHS/DestinySeasonDefinition.json';
 import DestinySourceDefinition from '@Data/manifest/zh-CHS/DestinySourceDefinition.json';
+import DestinyStatDefinition from '@Data/manifest/zh-CHS/DestinyStatDefinition.json';
 import DestinyTraitDefinition from '@Data/manifest/zh-CHS/DestinyTraitDefinition.json';
+
+import DestinyInventoryItemDefinitionColloquial from '@Data/manifest/zh-CHS/DestinyInventoryItemDefinitionColloquial.json';
 
 const zhCHS = {
   definitions: {
+    BraytechActivityDefinition,
     BraytechActivityDifficultyDefinition,
     BraytechCommonDefinition,
     BraytechFeatureDefinition,
     BraytechMapTypesDefinition,
     BraytechRotationDefinition,
+    DestinyActivityDefinition,
+    DestinyActivityModeDefinition,
     DestinyActivityModifierDefinition,
     DestinyChecklistDefinition,
     DestinyCollectibleDefinition,
+    DestinyDamageTypeDefinition,
     DestinyHistoricalStatsDefinition,
+    DestinyInventoryItemDefinition,
     DestinyInventoryBucketDefinition,
-    DestinyInventoryItemDefinitionColloquial,
     DestinyMilestoneDefinition,
+    DestinySeasonDefinition,
     DestinySourceDefinition,
+    DestinyStatDefinition,
     DestinyTraitDefinition,
+  },
+  optional: {
+    colloquialDefinitions: {
+      DestinyInventoryItemDefinition: DestinyInventoryItemDefinitionColloquial,
+    },
   },
 };
 
