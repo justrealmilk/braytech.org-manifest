3760b4805ea7c0ba89843224ed81052ec3381d30
lieuweberg <36763108+lieuweberg@users.noreply.github.com>
2023-07-24 15:45:18 +0200

    update en-OwO (#249)

--- en-OwO/index.ts
+++ en-OwO/index.ts
@@ -22,6 +22,7 @@ import DestinyPhaseDefinition from '@Data/manifest/en-OwO/DestinyPhaseDefinition
 import DestinyPresentationNodeDefinition from '@Data/manifest/en-OwO/DestinyPresentationNodeDefinition.json';
 import DestinyRecordDefinition from '@Data/manifest/en-OwO/DestinyRecordDefinition.json';
 import DestinySeasonDefinition from '@Data/manifest/en-OwO/DestinySeasonDefinition.json';
+import DestinySocketCategoryDefinition from '@Data/manifest/en-OwO/DestinySocketCategoryDefinition.json';
 import DestinySourceDefinition from '@Data/manifest/en-OwO/DestinySourceDefinition.json';
 import DestinyStatDefinition from '@Data/manifest/en-OwO/DestinyStatDefinition.json';
 import DestinyTraitDefinition from '@Data/manifest/en-OwO/DestinyTraitDefinition.json';
@@ -55,6 +56,7 @@ const enOwO = {
     DestinyPresentationNodeDefinition,
     DestinyRecordDefinition,
     DestinySeasonDefinition,
+    DestinySocketCategoryDefinition,
     DestinySourceDefinition,
     DestinyStatDefinition,
     DestinyTraitDefinition,
