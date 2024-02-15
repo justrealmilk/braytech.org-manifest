fa8bed86a8e8c3e3fe85663a2c56323338924bd1
Tom Chapman <chapman@live.com.au>
2024-02-15 11:41:49 +1000

    ...

--- pt-BR/index.ts
+++ pt-BR/index.ts
@@ -1,8 +1,8 @@
-import BraytechCommonDefinition from '@Data/manifest/pt-BR/BraytechCommonDefinition.json';
-import BraytechFeatureDefinition from '@Data/manifest/pt-BR/BraytechFeatureDefinition.json';
-import BraytechHelpDefinition from '@Data/manifest/pt-BR/BraytechHelpDefinition.json';
-import DestinyActivityModifierDefinition from '@Data/manifest/pt-BR/DestinyActivityModifierDefinition.json';
-import DestinyChecklistDefinition from '@Data/manifest/pt-BR/DestinyChecklistDefinition.json';
+import BraytechCommonDefinition from '@/src/data/manifest/pt-BR/BraytechCommonDefinition.json';
+import BraytechFeatureDefinition from '@/src/data/manifest/pt-BR/BraytechFeatureDefinition.json';
+import BraytechHelpDefinition from '@/src/data/manifest/pt-BR/BraytechHelpDefinition.json';
+import DestinyActivityModifierDefinition from '@/src/data/manifest/pt-BR/DestinyActivityModifierDefinition.json';
+import DestinyChecklistDefinition from '@/src/data/manifest/pt-BR/DestinyChecklistDefinition.json';
 
 const ptBR = {
   definitions: {
